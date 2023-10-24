52a6faa63f0bb7a18a8b6e16ea40b6869bd00b54
Fabian Gloor <fabian.marc.gloor@gmail.com>
2023-10-24 09:50:27 +0200

    workday save endpoint unexpected save error handling

--- src/controllers/WorkdayController.ts
+++ src/controllers/WorkdayController.ts
@@ -22,32 +22,37 @@ const save = async (req: Request, res: Response, next: NextFunction) => {
     const dateOnly = toDateOnly(new Date(otherFields.date));
     const { start, end } = getWeekDateRange(new Date(dateOnly));
 
-    sequelize.transaction(async (transaction) => {
+    await sequelize.transaction(async (transaction) => {
       // Find/create workweek
-      const [{ id: workweekId }] = await Workweek.findOrCreate({
+      const [workweek, wasCreated] = await Workweek.findOrCreate({
         where: { date: { [Op.between]: [start, end] } },
         defaults: { userId, start, end },
         transaction
       });
 
+      if (!workweek || (workweek && !workweek.id && !wasCreated)) {
+        return Promise.reject(res.status(500).send('Workweek operation failed.')); // Sequelize auto transaction rollback
+      }
+
       // Update/create workday
       const workday = await Workday.findOne({
-        where: { workweekId, date: dateOnly },
+        where: { workweekId: workweek.id, date: dateOnly },
         transaction
       });
       if (workday) {
         const editableFields = filterModelFields(otherFields, editableWorkdayFields);
-        await workday.update(editableFields, { transaction });
+        const result = await workday.update(editableFields, { transaction });
+        if (!result) return Promise.reject(res.status(500).send('Workday update failed.')); // Sequelize auto transaction rollback
       } else {
-        await Workday.create({
-          workweekId,
+        const result = await Workday.create({
+          workweekId: workweek.id,
           ...otherFields,
           date: dateOnly
         }, { transaction });
+        if (!result) return Promise.reject(res.status(500).send('Workday creation failed.')); // Sequelize auto transaction rollback
       }
     });
 
-    // Send response
     res.status(200).send('Workday save succeeded.');
   } catch (err) {
     console.error(`${serverError} Error: ${err}`);
